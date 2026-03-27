77c29277fb12554abb00c7ff23bd37ca14eef8e1
ChaitanyaGorrepati <chaitanyanaidu2006@gmail.com>
2026-03-27 17:44:56 +0530

    Delete Front-end directory

--- Front-end/models/userModel.js
+++
@@ -1,9 +0,0 @@
-import { users } from "../data/mockData.js";
-
-export function getUsers() {
-  return users;
-}
-
-export function getOfficers() {
-  return users.filter(u => u.role === "officer");
-}
